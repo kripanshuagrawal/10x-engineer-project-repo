371d17fdcaedc6e18fc26d738f297ed29e23dd9c
kripanshuagrawal <kripanshuagrawal@gmail.com>
2026-03-06 14:52:08 +0000

    Docker installations added

--- .devcontainer/setup.sh
+++ .devcontainer/setup.sh
@@ -5,6 +5,12 @@ CONFIG_DIR="$HOME/.continue"
 CONFIG_FILE="$CONFIG_DIR/config.yaml"
 API_KEY="sk-helicone-2v7akjq-m2xueay-ttt23kq-hx57ubq"
 
+echo "Installing Docker..."
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+rm get-docker.sh
+echo "Docker installed successfully."
+
 echo "Installing Python dependencies..."
 pip install --upgrade pip
 pip install -r requirements.txt
